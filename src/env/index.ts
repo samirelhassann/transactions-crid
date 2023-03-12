e5caaaae984b02bf76957736ce4ed792e5955461
Samir El Hassan <samir.hassan@ab-inbev.com>
2023-03-12 14:20:09 -0300

    chore: production ocnfig

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV === "test") {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["DEV", "PROD", "test"]).default("DEV"),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
